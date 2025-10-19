63977c8038cbffbb53892d5526a709329429ffbd
sailex <elias.hahn@mail.de>
2025-10-19 18:07:37 +0200

    ty to fix mixin

--- src/main/java/me/sailex/mixins/LivingEntityMixin.java
+++ src/main/java/me/sailex/mixins/LivingEntityMixin.java
@@ -4,8 +4,8 @@ import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Mixin({LivingEntity.class})
+@Mixin(LivingEntity.class)
 public interface LivingEntityMixin {
-   @Accessor(value="lastAttackedTicks")
+   @Accessor("lastAttackedTicks")
    int getLastAttackedTicks();
 }
